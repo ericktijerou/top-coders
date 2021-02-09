5d97567ec745106183c93f7eb1bf1273e730d831
Erick Tijero <ericktijerou@gmail.com>
2021-02-09 18:23:15 -0500

    Parse string to joined date

--- app/src/main/kotlin/com/ericktijerou/topcoders/ui/mapper/UserMapper.kt
+++ app/src/main/kotlin/com/ericktijerou/topcoders/ui/mapper/UserMapper.kt
@@ -3,6 +3,7 @@ package com.ericktijerou.topcoders.ui.mapper
 import com.ericktijerou.topcoders.R
 import com.ericktijerou.topcoders.domain.entity.User
 import com.ericktijerou.topcoders.ui.entity.UserView
+import com.ericktijerou.topcoders.ui.util.toJoinedDate
 
 fun User.toView(): UserView {
     val hasCompany = company.isNotEmpty()
@@ -11,7 +12,7 @@ fun User.toView(): UserView {
         username = username,
         avatarUrl = avatarUrl,
         bio = bio,
-        info = if (hasCompany) company else createdAt,
+        info = if (hasCompany) company else createdAt.toJoinedDate(),
         infoIcon = if (hasCompany) R.drawable.ic_business else R.drawable.ic_clock
     )
 }
--- app/src/main/kotlin/com/ericktijerou/topcoders/ui/util/Extensions.kt
+++ app/src/main/kotlin/com/ericktijerou/topcoders/ui/util/Extensions.kt
@@ -2,7 +2,23 @@ package com.ericktijerou.topcoders.ui.util
 
 import android.content.Context
 import android.widget.Toast
+import java.text.SimpleDateFormat
+import java.util.*
 
 fun Context.showErrorMessage(message: String) {
     Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+}
+
+fun String.toJoinedDate(): String {
+    return this.toDate()?.formatToViewDateDefaults().orEmpty()
+}
+
+fun String.toDate(): Date?  {
+    val dateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.getDefault())
+    return dateFormat.parse(this)
+}
+
+fun Date.formatToViewDateDefaults(): String {
+    val sdf= SimpleDateFormat("MMMM dd, yyyy", Locale.getDefault())
+    return sdf.format(this).capitalize(Locale.getDefault())
 }
