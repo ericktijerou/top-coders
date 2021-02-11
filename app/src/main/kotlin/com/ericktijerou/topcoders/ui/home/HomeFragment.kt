9d95cc69c1e5e3aa583f03f4c9ae8410c60d8942
Erick Tijero <ericktijerou@gmail.com>
2021-02-11 17:57:40 -0500

    Add missing resource

--- app/src/main/kotlin/com/ericktijerou/topcoders/ui/home/HomeFragment.kt
+++ app/src/main/kotlin/com/ericktijerou/topcoders/ui/home/HomeFragment.kt
@@ -1,94 +1,41 @@
 package com.ericktijerou.topcoders.ui.home
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
-import androidx.lifecycle.observe
-import androidx.paging.LoadState
-import androidx.recyclerview.widget.DividerItemDecoration
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.ericktijerou.topcoders.R
 import com.ericktijerou.topcoders.databinding.FragmentHomeBinding
-import com.ericktijerou.topcoders.ui.entity.UserView
-import com.ericktijerou.topcoders.ui.home.adapter.UserItemListener
-import com.ericktijerou.topcoders.ui.home.adapter.UserListAdapter
-import com.ericktijerou.topcoders.ui.home.adapter.UserLoadStateAdapter
+import com.ericktijerou.topcoders.ui.home.coder.CoderHomeFragment
+import com.ericktijerou.topcoders.ui.home.repo.RepoHomeFragment
+import com.ericktijerou.topcoders.ui.util.PageTitleProvider
 import com.ericktijerou.topcoders.ui.util.dataBinding
-import com.ericktijerou.topcoders.ui.util.showErrorMessage
-import dagger.hilt.android.AndroidEntryPoint
+import com.ericktijerou.topcoders.ui.util.setupWithViewPager2
 
-@AndroidEntryPoint
-class HomeFragment : Fragment(R.layout.fragment_home), UserItemListener,
-    SwipeRefreshLayout.OnRefreshListener {
+class HomeFragment : Fragment(R.layout.fragment_home) {
 
-    private val viewModel: HomeViewModel by viewModels()
     private val binding: FragmentHomeBinding by dataBinding()
-    private val adapter: UserListAdapter = UserListAdapter(this)
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        initUI()
-        obsUsers()
+        initViewPager()
     }
 
-    override fun onPause() {
-        adapter.removeLoadStateListener {
-            Log.e("MainFragmet", "LoadStateListener Removed")
-        }
-        super.onPause()
-    }
-
-    private fun initUI() {
+    private fun initViewPager() {
         with(binding) {
-            srlRefresh.setOnRefreshListener(this@HomeFragment)
-            initAdapterLoadingState()
-            rvUsers.addItemDecoration(
-                DividerItemDecoration(
-                    requireContext(),
-                    DividerItemDecoration.VERTICAL
-                )
+            val items = listOf(
+                getString(R.string.label_coder) to CoderHomeFragment(),
+                getString(R.string.label_repo) to RepoHomeFragment()
             )
-            rvUsers.adapter =
-                adapter.withLoadStateFooter(footer = UserLoadStateAdapter { adapter.retry() })
-            executePendingBindings()
+            homePager.adapter = HomeAdapter(this@HomeFragment, items)
+            tabLayout.setupWithViewPager2(homePager)
         }
     }
 
-    private fun initAdapterLoadingState() {
-        adapter.addLoadStateListener { loadState ->
-            if (loadState.refresh is LoadState.Loading) {
-                binding.srlRefresh.isRefreshing = true
-            } else {
-                binding.srlRefresh.isRefreshing = false
-
-                val errorState = when {
-                    loadState.append is LoadState.Error -> loadState.append as LoadState.Error
-                    loadState.refresh is LoadState.Error -> loadState.refresh as LoadState.Error
-                    else -> null
-                }
-                errorState?.let { requireContext().showErrorMessage(it.error.message ?: "Error") }
-            }
-        }
+    inner class HomeAdapter(fragment: Fragment, private val items: List<Pair<String, Fragment>>) :
+        FragmentStateAdapter(fragment), PageTitleProvider {
+        override fun getItemCount(): Int = items.size
+        override fun createFragment(position: Int): Fragment = items[position].second
+        override fun getPageTitle(position: Int): String? = items[position].first
     }
 
-    private fun obsUsers() {
-        viewModel.userList.observe(viewLifecycleOwner) {
-            adapter.submitData(viewLifecycleOwner.lifecycle, it)
-        }
-    }
-
-    override fun onUserClick(item: UserView) {
-        // EMPTY
-    }
-
-    override fun onRefresh() {
-        adapter.refresh()
-    }
-
-    override fun onDestroyView() {
-        if (binding.rvUsers.adapter != null)
-            binding.rvUsers.adapter = null
-        super.onDestroyView()
-    }
 }
