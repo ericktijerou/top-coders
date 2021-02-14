5be61075a3b0db90c34c3b9bf1abd5648560c5de
Erick Tijero <ericktijerou@gmail.com>
2021-02-14 11:26:40 -0500

    Show repo list

--- app/src/main/kotlin/com/ericktijerou/topcoders/ui/util/BindingAdapters.kt
+++ app/src/main/kotlin/com/ericktijerou/topcoders/ui/util/BindingAdapters.kt
@@ -1,5 +1,6 @@
 package com.ericktijerou.topcoders.ui.util
 
+import android.graphics.Color
 import android.widget.ImageView
 import androidx.annotation.DrawableRes
 import androidx.databinding.BindingAdapter
@@ -16,5 +17,16 @@ object BindingAdapters {
     @JvmStatic
     fun src(view: ImageView, @DrawableRes drawableRes: Int) {
         view.setImageResource(drawableRes)
+    }
+
+    @BindingAdapter("app:textColor")
+    @JvmStatic
+    fun textColor(view: ImageView, color: String) {
+        val colorInt = if (color.isNotEmpty()) {
+            Color.parseColor(color)
+        } else {
+            view.context.getAttributeColor(android.R.attr.textColorSecondary)
+        }
+        view.setBackgroundColor(colorInt)
     }
 }
